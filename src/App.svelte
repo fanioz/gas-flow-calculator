e7e61eb4eb277b80ad35ddca3cacab98f72f8e2d
fanioz <fanio.zilla@gmail.com>
2020-03-29 20:16:46 +0800

    Create calculation form

--- src/App.svelte
+++ src/App.svelte
@@ -10,39 +10,37 @@
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
-      ZEIT Now
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/zeit/now/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
+  <h1>Simple Gas Flow Calculator</h1>
+  <h2>Current date:</h2>
   <p>{date ? date : 'Loading date...'}</p>
+  <br>
+  <table>
+        <th colspan="3">Simple Gas Flow Calculator</th>
+        <tr>
+            <td>Pipe Diameter:</td>
+            <td><input type="radio" id="line3" name="lined" checked="true"><label for="line3">Line 3"</label></td>
+            <td><input type="radio" id="line6" name="lined"><label for="line6">Line 6"</label></td>
+        </tr>
+        <tr>
+            <td><label for="dOrificeIn">Orifice D:</label></td>
+            <td><input type="text" id="dOrificeIn" name="dOrificeIn" oninput="ConvertMM()">inch</td>
+            <td><input type="text" id="dOrificeMM" name="dOrificeMM" value="50.8" >mm</td>
+        </tr>
+        <tr>
+            <td><label for="upPress">Pressure:</label></td>
+            <td colspan="2"><input type="text" id="upPress" name="press" placeholder="Bar" value="50"></td>
+        </tr>
+        <tr>
+            <td><label for="hw">HW:</label></td>
+            <td  colspan="2"><input type="text" id="hw" name = "hw" placeholder="inchH20" value="50"></td>
+        </tr>
+        <tr>
+            <td><label for="temp">Temperature:</label></td>
+            <td  colspan="2"><input type="text" id="temp" name="temp" placeholder="deg C" value="50"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><center><button onclick="Calculate()">Hitung</button></center></td>
+            <td><p id="Result">Result :</p></td>
+        </tr>
+  </table>
 </main>
