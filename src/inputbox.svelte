d244a7d85bec24c3ed40031fa24c19ec0430f460
fanioz <fanio.zilla@gmail.com>
2020-03-30 20:00:11 +0800

    Add component inputbox

---
+++ src/inputbox.svelte
@@ -0,0 +1,6 @@
+<script>
+export let caption = "no caption", variabel = 0, endunit = "", min=0, max=100, step=1;
+</script>
+<label>{caption}
+    <input type=number bind:value={variabel} {min} {max} {step}> {endunit}
+</label>
