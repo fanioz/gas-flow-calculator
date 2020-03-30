76f1687badd156d2912556d4d95321222f31caf6
fanioz <fanio.zilla@gmail.com>
2020-03-31 06:06:57 +0800

    Add: On Input event forwarder

--- src/inputbox.svelte
+++ src/inputbox.svelte
@@ -1,6 +1,7 @@
 <script>
 export let caption = "no caption", variabel = 0, endunit = "", min=0, max=100, step=1;
+
 </script>
 <label>{caption}
-    <input type=number bind:value={variabel} {min} {max} {step}> {endunit}
+    <input type=number bind:value={variabel} {min} {max} {step} on:input> {endunit}
 </label>
